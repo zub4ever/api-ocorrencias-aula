cf2193a76713a6f441c948a55cff8d654cb43233
Alisson Nascimento de Souza <sonda.alissonns@tjac.jus.br>
2026-05-13 15:14:31 -0500

    Corrige a porta do serviço PostgreSQL para 5432 no app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { OcorrenciasModule } from './ocorrencias/ocorrencias.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST || 'postgres',
-      port: Number(process.env.POSTGRES_PORT || 3652),
+      port: Number(process.env.POSTGRES_PORT || 5432),
       username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'postgres',
       database: process.env.POSTGRES_DB || 'ocorrencias',
